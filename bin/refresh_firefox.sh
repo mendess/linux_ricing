bc11d5c51599950a66ac0ee064d1a5f3982194d2
mendess <pedro.mendes.26@gmail.com>
2020-11-26 09:44:02 +0000

    Improve refresh firefox

--- bin/refresh_firefox.sh
+++ bin/refresh_firefox.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-CURRENT_WID=$(xdotool getwindowfocus)
-
 WID=$(xdotool search --name "Mozilla Firefox")
-xdotool windowactivate "$WID"
-xdotool key F5
-
-xdotool windowactivate "$CURRENT_WID"
+xdotool key --window "$WID" F5
